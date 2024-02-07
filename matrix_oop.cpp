78035707c401835a7572a5a11454f2aebb80a20a
Nikolas-Svet <nikitaswetckin@mail.ru>
2024-02-07 21:53:52 +0700

    First commit

---
+++ matrix_oop.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Никита Светкин on 07.02.2024.
+//
+
+#include "matrix_oop.h"
---
+++ matrix_oop.h
@@ -0,0 +1,14 @@
+//
+// Created by Никита Светкин on 07.02.2024.
+//
+
+#ifndef UNTITLED3_MATRIX_OOP_H
+#define UNTITLED3_MATRIX_OOP_H
+
+
+class matrix_oop {
+
+};
+
+
+#endif //UNTITLED3_MATRIX_OOP_H
