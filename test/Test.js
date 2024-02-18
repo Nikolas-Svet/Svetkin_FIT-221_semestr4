0b3b77d5a7ce4d34ff7d5389962959de2f119f3b
Nikolas-Svet <nikitaswetckin@mail.ru>
2024-02-18 14:00:04 +0700

    first commit

---
+++ test/Test.js
@@ -0,0 +1,19 @@
+// Объявление переменных с различными типами данных
+let var1 = 10;                 // Целое число
+let var2 = "Hello, world!";    // Строка
+let var3 = true;               // Булевое значение
+let var4 = {                   // Объект
+  name: "John",
+  age: 30
+};
+let var5 = [1, 2, 3, 4, 5];    // Массив
+
+// Вывод типов переменных на экран с помощью функции typeof()
+console.log("Тип переменной var1:", typeof(var1));
+console.log("Тип переменной var2:", typeof(var2));
+console.log("Тип переменной var3:", typeof(var3));
+console.log("Тип переменной var4:", typeof(var4));
+console.log("Тип переменной var5:", typeof(var5));
+
+
+
---
+++ test/xample.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+describe('Example', function() {
+  it('should return true when value is true', function() {
+    assert.strictEqual(true, true);
+  });
+
+  it('should return false when value is false', function() {
+    assert.strictEqual(false, false);
+  });
+});
